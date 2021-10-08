e9c22d3eb1b0865f45f24d964699257a1204bf57
hanyk <hanyk9656@gmail.com>
2021-10-08 12:16:41 +0900

    mongDB connection

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => { //root directory에 오면 "Hello World!" cnffur
-  res.send('Hello World!안녕하세요')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on ${port}!`)
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://hanyk:dbsrud12@boilerplate.gwosc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 })
+.then(() => console.log('MongoDB Connected...'))
+.catch((err) => console.log('MongoDB Connected...',err))
+
+app.get('/', (req, res) => res.send('Hello World!안녕하세요'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
